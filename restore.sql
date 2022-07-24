542646ec1d73636475bbcf32f815b0c1fd005023
Jose Pablo Villa <47614932+jpvillaa@users.noreply.github.com>
2022-07-24 18:58:46 -0500

    Add files via upload

--- restore.sql
+++ restore.sql
@@ -22,7 +22,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE dvdrental;
 --
 -- Name: dvdrental; Type: DATABASE; Schema: -; Owner: postgres
 --
