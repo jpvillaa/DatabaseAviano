797b3af363920a08d32b6f6272896f655261c8f4
Jose Pablo Villa <47614932+jpvillaa@users.noreply.github.com>
2022-07-24 18:02:35 -0500

    Update restore.sql

--- restore.sql
+++ restore.sql
@@ -21,8 +21,6 @@ SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
-
-DROP DATABASE IF EXISTS dvdrental;  
 --
 -- Name: dvdrental; Type: DATABASE; Schema: -; Owner: postgres
 --
